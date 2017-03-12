2ee5b88e8ac0eaea7ad353069db8eebec69d42de
Michael Ray <michaelburtonray@gmail.com>
2017-03-11 19:33:29 -0500

    legislation page

--- src/Legislation.js
+++ src/Legislation.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './legislation.css';
 
 class Legislation extends Component {
   render() {
     return (
       <div className="legislation-page">
-        <p>This is the legislation page</p>
+
+        <iframe src="https://docs.google.com/document/d/1S-UXmIM7OIP7O6fr6MAY-I-bj_RhlhVVVjH-1NkHhuQ/pub?embedded=true"></iframe>
+
       </div>
     )
   }
