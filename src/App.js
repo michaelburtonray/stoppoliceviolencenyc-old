1f69ae0abde88652968dd0f4560297e0f62cd552
Michael Ray <michaelburtonray@gmail.com>
2017-03-18 13:25:46 -0400

    mobile navigation

--- src/App.js
+++ src/App.js
@@ -11,17 +11,39 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-
     this.ACTIVE = { color: 'white' }
+
+    this.state = {
+      mobileNavIsActive: false
+    }
+
+    this.toggleActiveMobileNav = this.toggleActiveMobileNav.bind(this);
   }
+
+  toggleActiveMobileNav(event) {
+    this.setState({ mobileNavIsActive: !this.state.mobileNavIsActive });
+  }
+
+  getMobileNavClassList() {
+    return ['mainnav--mobile', this.state.mobileNavIsActive? 'mainnav--active' : ''].join(' ')
+  }
+
+  getAppClassList() {
+    return ["App", this.state.mobileNavIsActive? 'mainnav--active' : ''].join(' ')
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className={this.getAppClassList()}>
+
         <header className="header">
           <div className="content header__content">
-            <IndexLink to="/">
-              <img src={logo} className="logo" alt="logo" />
+
+            <IndexLink className="logo" to="/">
+              <img src={logo} alt="logo" />
             </IndexLink>
+
+            <i className="fa fa-bars header__bars" aria-hidden="true" onClick={this.toggleActiveMobileNav}></i>
 
             <nav className="mainnav">
               <IndexLink className="mainnav-anchor" activeStyle={this.ACTIVE} to="/">Why An Elected Board?</IndexLink>
@@ -31,8 +53,18 @@ class App extends Component {
               <Link className="mainnav-anchor" activeStyle={this.ACTIVE} to="/contact">Contact</Link>
               <a className="mainnav-anchor button green-on-orange" href="//gofundme.com/ecrbnyc" target="_blank">Donate Now!</a>
             </nav>
+
           </div>
         </header>
+
+        <nav className={this.getMobileNavClassList()}>
+          <IndexLink className="mainnav-anchor" activeStyle={this.ACTIVE} to="/">Why An Elected Board?</IndexLink>
+          <Link className="mainnav-anchor" activeStyle={this.ACTIVE} to="/join">Join the Campaign</Link>
+          <Link className="mainnav-anchor" activeStyle={this.ACTIVE} to="/legislation">The Legislation</Link>
+          <Link className="mainnav-anchor" activeStyle={this.ACTIVE} to="/faq">FAQ</Link>
+          <Link className="mainnav-anchor" activeStyle={this.ACTIVE} to="/contact">Contact</Link>
+          <a className="mainnav-anchor button green-on-orange" href="//gofundme.com/ecrbnyc" target="_blank">Donate Now!</a>
+        </nav>
 
         <div className="wrapper">
           <div className="content">
@@ -51,8 +83,8 @@ class App extends Component {
 
           <div className="footer-bottom">
             <div className="content footer-bottom__content">
-              <a  href="/">
-                <img src={logoBlue} className="logo" alt="logo" />            
+              <a  className="logo" href="/">
+                <img src={logoBlue} alt="logo" />            
               </a>
 
               <form className="subscribe-form">
@@ -78,6 +110,7 @@ class App extends Component {
             </div>
           </div>
         </footer>
+
       </div>
     );
   }
