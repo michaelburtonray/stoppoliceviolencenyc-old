c0004b9c9ce1e4b50cad49b73a0edc0bb253bd2c
kikibox <ksb93@cornell.edu>
2019-10-15 13:54:49 -0400

    added twitter button

--- src/App.js
+++ src/App.js
@@ -458,6 +458,15 @@ class App extends Component {
                 <i className="fa fa-instagram" aria-hidden="true" />
                 <span>instagram</span>
               </a>
+			           <a
+                className="footer-social-anchor"
+                href="//twitter.com/ecrbfornyc"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-twitter" aria-hidden="true" />
+                <span>instagram</span>
+              </a>
             </nav>
           </div>
 
